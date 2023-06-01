2085e2218e473cfc2f2c1fef0750bdcbf258b81b
Joe Glanville <josephglanville@gmail.com>
2023-06-01 11:45:31 +0100

    Update to use Transcient Props to fix error

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 
+type TranscientProps = {
+  $buttonType: 'primary' | 'button';
+};
+
 interface Props {
   type: 'primary' | 'button';
   children: React.ReactNode;
@@ -9,18 +13,18 @@ interface Props {
 
 const Button = ({ type, children, onClick }: Props) => {
   return (
-    <ButtonElement buttonType={type} onClick={onClick}>
+    <ButtonElement $buttonType={type} onClick={onClick}>
       {children}
     </ButtonElement>
   );
 };
 
-const ButtonElement = styled.button<{ buttonType: 'primary' | 'button' }>`
+const ButtonElement = styled.button<TranscientProps>`
   background-color: ${(props) =>
-    props.buttonType === 'primary' ? 'var(--bright-blue)' : 'none'};
+    props.$buttonType === 'primary' ? 'var(--bright-blue)' : 'none'};
   color: ${(props) =>
-    props.buttonType === 'primary' ? 'white' : 'var(--desaturated-blue)'};
-  padding: ${(props) => (props.buttonType === 'primary' ? '1rem' : '0')};
+    props.$buttonType === 'primary' ? 'white' : 'var(--desaturated-blue)'};
+  padding: ${(props) => (props.$buttonType === 'primary' ? '1rem' : '0')};
   border-radius: 15px;
   width: 100%;
   text-align: center;
@@ -28,17 +32,17 @@ const ButtonElement = styled.button<{ buttonType: 'primary' | 'button' }>`
   font-size: 0.85rem;
   letter-spacing: 0.5px;
   box-shadow: ${(props) =>
-    props.buttonType === 'primary'
+    props.$buttonType === 'primary'
       ? '0px 10px 20px rgba(116, 108, 233, 0.4)'
       : 'none'};
   &:hover {
     cursor: pointer;
     background-color: ${(props) =>
-      props.buttonType === 'primary'
+      props.$buttonType === 'primary'
         ? 'var(--desaturated-bright-blue)'
         : 'none'};
     color: ${(props) =>
-      props.buttonType === 'primary' ? 'white' : 'var(--dark-blue)'};
+      props.$buttonType === 'primary' ? 'white' : 'var(--dark-blue)'};
   }
 `;
 
